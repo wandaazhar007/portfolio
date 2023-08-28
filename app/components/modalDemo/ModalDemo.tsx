a4b88cc15fd0be882b30bd12e7c3ef3b1d7e74a0
wanda azhar <wandaazhar@gmail.com>
2023-08-28 17:47:18 -0400

    refactor code

--- app/components/modalDemo/ModalDemo.tsx
+++ app/components/modalDemo/ModalDemo.tsx
@@ -10,7 +10,6 @@ import Link from 'next/link';
 const ModalDemo: any = ({ openModal, closeModal, propsId }: any) => {
   if (!openModal) return null;
 
-  // const [myWork, setMyWork] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [name, setName] = useState("");
   const [desc, setDesc] = useState("");
@@ -47,97 +46,68 @@ const ModalDemo: any = ({ openModal, closeModal, propsId }: any) => {
       <section className="modalDemo">
         <div className="wrapContainer">
           {isLoading ? (
-            <>
-              <div className="boxContainer">
-                <div className="modalBody">
-                  <div className="btnClose" onClick={closeModal}></div>
-                  <div className="image skeleton skeleton-image-modal">
-                    {/* <Image src="/ss_admin_kansha.png" alt='image' width={100} height={100} /> */}
-                  </div>
-                  <div className="content">
-                    <h1 className="title skeleton skeleton-title-modal"></h1>
-                    <p className="desc skeleton skeleton-desc-modal"></p>
-                    <p className="desc skeleton skeleton-desc-modal"></p>
-                    <p className="desc skeleton skeleton-desc-modal"></p>
-                    <div className="tag">
-                      <button className="skeleton skeletonBtnTag"></button>
-                      <button className="skeleton skeletonBtnTag"></button>
-                      <button className="skeleton skeletonBtnTag"></button>
-                    </div>
-                    <div className="buttonGroup">
-                      <Link href="#" target='_blank'>
-                        <button className="btn btnPreview skeleton skeletonBtnModal"> Live Preview</button>
-                      </Link>
-                      <Link href="#" target='_blank'>
-                        <button className="btn btnGithub skeleton skeletonBtnModal">Repository</button>
-                      </Link>
-                    </div>
-                  </div>
+            <div className="boxContainer">
+              <div className="modalBody">
+                <div className="btnClose" onClick={closeModal}></div>
+                <div className="image skeleton skeleton-image-modal">
+
                 </div>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className="boxContainer">
-                <div className="modalBody">
-                  <div className="btnClose" onClick={closeModal}></div>
-                  <div className="image">
-                    <Image src={urlImage} alt='image' width={500} height={500} />
+                <div className="content">
+                  <h1 className="title skeleton skeleton-title-modal"></h1>
+                  <p className="desc skeleton skeleton-desc-modal"></p>
+                  <p className="desc skeleton skeleton-desc-modal"></p>
+                  <p className="desc skeleton skeleton-desc-modal"></p>
+                  <div className="tag">
+                    <button className="skeleton skeletonBtnTag"></button>
+                    <button className="skeleton skeletonBtnTag"></button>
+                    <button className="skeleton skeletonBtnTag"></button>
                   </div>
-                  <div className="content">
-                    <h1 className="title">{name}</h1>
-                    <p className="desc">{desc}</p>
-                    <div className="tag">
-                      <button className="btnTag">ReactJs</button>
-                      <button className="btnTag">NextJs</button>
-                    </div>
-                    <div className="buttonGroup">
-                      {type === 'backend' ? (
-                        <Link href="#">
-                          <button className="btn btnPreviewDisable"><FontAwesomeIcon icon={faClose} className='icon' /> No Preview</button>
-                        </Link>
-                      ) : (
-                        <Link href={preview} target='_blank'>
-                          <button className="btn btnPreview"><FontAwesomeIcon icon={faDashboard} className='icon' /> Preview</button>
-                        </Link>
-                      )}
-                      <Link href={github} target='_blank'>
-                        <button className="btn btnGithub"><FontAwesomeIcon icon={faGithub} className='icon' /> Repository</button>
-                      </Link>
-                    </div>
-                    <div className="license">
-                      <p><FontAwesomeIcon icon={faScaleBalanced} className='icon' /> {license}</p>
-                    </div>
+                  <div className="buttonGroup">
+                    <Link href="#" target='_blank'>
+                      <button className="btn btnPreview skeleton skeletonBtnModal"> Live Preview</button>
+                    </Link>
+                    <Link href="#" target='_blank'>
+                      <button className="btn btnGithub skeleton skeletonBtnModal">Repository</button>
+                    </Link>
                   </div>
-                </div>
-              </div>
-            </>
-          )}
-          {/* <div className="boxContainer">
-            <div className="modalBody">
-              <div className="btnClose" onClick={closeModal}></div>
-              <div className="image">
-                <Image src="/ss_admin_kansha.png" alt='image' width={100} height={100} />
-              </div>
-              <div className="content">
-                <h1 className="title">{name}test</h1>
-                <p className="desc">{desc}</p>
-                <div className="tag">
-                  <button className="btnTag">ReactJs</button>
-                  <button className="btnTag">NextJs</button>
-                </div>
-                <div className="footerModal">
-                  <Link href={preview} target='_blank'>
-                    <button className="btn btnPreview"><FontAwesomeIcon icon={faEye} className='icon' /> Live Preview</button>
-                  </Link>
-                  <Link href={github} target='_blank'>
-                    <button className="btn btnGithub"><FontAwesomeIcon icon={faGithub} className='icon' /> Repository</button>
-                  </Link>
                 </div>
               </div>
             </div>
-          </div> */}
-
+          ) : (
+            <div className="boxContainer">
+              <div className="modalBody">
+                <div className="btnClose" onClick={closeModal}></div>
+                <div className="image">
+                  <Image src={urlImage} alt='image' width={500} height={500} />
+                </div>
+                <div className="content">
+                  <h1 className="title">{name}</h1>
+                  <p className="desc">{desc}</p>
+                  <div className="tag">
+                    <button className="btnTag">ReactJs</button>
+                    <button className="btnTag">NextJs</button>
+                  </div>
+                  <div className="buttonGroup">
+                    {type === 'backend' ? (
+                      <Link href="#">
+                        <button className="btn btnPreviewDisable"><FontAwesomeIcon icon={faClose} className='icon' /> No Preview</button>
+                      </Link>
+                    ) : (
+                      <Link href={preview} target='_blank'>
+                        <button className="btn btnPreview"><FontAwesomeIcon icon={faDashboard} className='icon' /> Preview</button>
+                      </Link>
+                    )}
+                    <Link href={github} target='_blank'>
+                      <button className="btn btnGithub"><FontAwesomeIcon icon={faGithub} className='icon' /> Repository</button>
+                    </Link>
+                  </div>
+                  <div className="license">
+                    <p><FontAwesomeIcon icon={faScaleBalanced} className='icon' /> {license}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </section>
 
