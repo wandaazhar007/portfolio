fce579a51dec92fa19d9d2cb3adcf3492847e65c
wanda azhar <wandaazhar@gmail.com>
2023-08-28 16:41:18 -0400

    refactor code

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -19,7 +19,7 @@ const Hero: React.FC = () => {
     setIsLoading(true);
   }
   return (
-    <section className="hero">
+    <section className="home">
       <div className="wrapContainer">
         <div className="sectionContainer">
 
@@ -34,9 +34,20 @@ const Hero: React.FC = () => {
                 scale: [0, 1.2, 1],
                 rotate: [0, -3, 0]
               }}
-            >I do more than just build a website</motion.h1>
-            <p > I work for an advertising digital agency as a front-end web designer. currently, I&apos;m not looking for a job but I love to help small businesses grow especially with websites or landing pages</p>
-            <div className="proof">
+            >I do more than just build a website
+            </motion.h1>
+            <motion.p
+              initial={{ opacity: 0, scale: 1, x: -100 }}
+              whileInView={{ opacity: 1, scale: 1, x: 0 }}
+              transition={{ duration: 1 }}
+            >
+              I work for an advertising digital agency as a front-end web designer. currently, I&apos;m not looking for a job but I love to help small businesses grow especially with websites or landing pages
+            </motion.p>
+            <motion.div
+              initial={{ opacity: 0, scale: 1, y: 100 }}
+              whileInView={{ opacity: 1, scale: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              className="proof">
               <div className="icons">
                 <FontAwesomeIcon icon={faStar} className="icon" />
                 <FontAwesomeIcon icon={faStar} className="icon" />
@@ -44,12 +55,19 @@ const Hero: React.FC = () => {
                 <FontAwesomeIcon icon={faStar} className="icon" />
                 <FontAwesomeIcon icon={faStar} className="icon" />
               </div>
-              <span >Rated 5 stars on Fiverr</span>
-            </div>
-            <button>
-              <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" />
-              <span>My Fiverr Profile</span>
-            </button>
+              <span>Rated 5 stars on Fiverr</span>
+            </motion.div>
+            <Link href="https://fiverr.com/" target="_blank">
+              <motion.button
+                animate={{
+                  scale: [0, 1.2, 1],
+                  rotate: [0, -3, 0]
+                }}
+              >
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" />
+                <span>My Fiverr Profile</span>
+              </motion.button>
+            </Link>
           </div>
 
           <div className="experiences">
