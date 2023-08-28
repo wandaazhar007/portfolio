b2a5572f4439d31590ebb7faaf8e6283658c4f14
wanda azhar <wandaazhar@gmail.com>
2023-08-28 17:51:28 -0400

    refactor code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import './styles/globals.scss';
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Wanda Azhar | Portfolio',
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import Hero from './components/hero/Hero'
 
 export default function Home() {
