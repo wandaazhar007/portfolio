4aefd11bdbd27a527c9803a62297b27662271689
wanda azhar <wandaazhar@gmail.com>
2023-11-09 08:45:53 -0500

    update blog page

--- app/components/searchBlog/SearchBlog.tsx
+++ app/components/searchBlog/SearchBlog.tsx
@@ -1,10 +1,10 @@
 'use client'
 import './searchBlog.scss';
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useRef, useContext } from 'react';
 import axios from 'axios';
 import { SearchBlogContext } from '../../context//SearchBlogContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClose } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft, faAngleRight, faArrowLeft, faArrowRight, faClose } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBlog: React.FC = () => {
   // const [querySearch, setQuerySearch] = useState("");
@@ -22,34 +22,77 @@ const SearchBlog: React.FC = () => {
   //   console.log('first')
   // }
 
-  const getBlogPerCategory = async () => {
-    const responseSearch = await axios.get(`${process.env.NEXT_PUBLIC_GET_BLOG_PER_CATEGORY}?search_query=${btnPerCategory}`)
-    console.log(responseSearch.data.result);
-    console.log('first')
-  }
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuerySearch(e.target.value);
+    // setBtnPerCategory('')
     // getSearch();
   }
 
   const handlePerCategoryBtn = (id: number) => {
+    // setQuerySearch('');
     setBtnPerCategory(id)
-    // alert(id)
   }
   const handleReset = () => {
     setQuerySearch('')
+    setBtnPerCategory(3)
   }
+
+  const tabMenuRef = useRef<HTMLUListElement>(null);
+  const iconLeftRef = useRef<SVGSVGElement>(null);
+  const iconRightRef = useRef<SVGSVGElement>(null);
+
+  const handleClickLeft = () => {
+    tabMenuRef.current!.scrollLeft -= 100;
+    const scrollRightValue = Math.ceil(tabMenuRef.current!.scrollLeft);
+    console.log(scrollRightValue)
+    iconRightRef.current!.style.display = scrollRightValue <= 100 ? "block" : "none";
+  }
+
+  const handleClickRight = () => {
+    tabMenuRef.current!.scrollLeft += 100;
+    iconVisibility();
+  }
+
+  const iconVisibility = () => {
+    let scrollLeftValue = Math.ceil(tabMenuRef.current!.scrollLeft);
+    iconLeftRef.current!.style.display = scrollLeftValue > 0 ? "block" : "none"
+
+    let scrollableWidth = tabMenuRef.current!.scrollWidth - tabMenuRef.current!.clientWidth;
+
+  }
+
   return (
     <section className="search">
-      <input type='text' placeholder='search article here' onChange={handleSearch} value={querySearch} />
-      {querySearch && <span className='result'><button onClick={handleReset}>{querySearch} <FontAwesomeIcon icon={faClose} className='icon' /></button></span>}
-      <div className="buttonGroup">
-        <button className={btnPerCategory == 3 ? 'active' : ''} onClick={() => handlePerCategoryBtn(3)}>HTML</button>
+      <div className="wrapContainer">
+        <div className="searchGroup">
+          <input type='text' placeholder='search article here' onChange={handleSearch} value={querySearch} />
+          {querySearch && <span className='result'><button onClick={handleReset}>{querySearch} <FontAwesomeIcon icon={faClose} className='icon' /></button></span>}
+        </div>
+
+
+        <div className="tabNavigation">
+          <FontAwesomeIcon icon={faAngleLeft} className="leftBtn" ref={iconLeftRef} onClick={handleClickLeft} />
+          <FontAwesomeIcon icon={faAngleRight} className="rightBtn" ref={iconRightRef} onClick={handleClickRight} />
+
+          <ul className="tabMenu" ref={tabMenuRef}>
+            <li className={`tabButton  ${btnPerCategory == 1 ? 'active' : ''}`} onClick={() => handlePerCategoryBtn(1)}>HTML</li>
+            <li className={`${btnPerCategory == 2 ? 'active' : ''} tabButton`} onClick={() => handlePerCategoryBtn(2)}>CSS</li>
+            <li className={`${btnPerCategory == 3 ? 'active' : ''} tabButton`} onClick={() => handlePerCategoryBtn(3)}>JavaScript</li>
+            <li className={`tabButton  ${btnPerCategory == 4 ? 'active' : ''}`} onClick={() => handlePerCategoryBtn(4)}>ReactJs</li>
+            <li className={`${btnPerCategory == 5 ? 'active' : ''} tabButton`} onClick={() => handlePerCategoryBtn(5)}>Git & GitHub</li>
+            <li className={`${btnPerCategory == 6 ? 'active' : ''} tabButton`} onClick={() => handlePerCategoryBtn(6)}>Linux Server</li>
+          </ul>
+        </div>
+
+
+        {/* <div className="buttonGroup">
+        <button className={btnPerCategory == 1 ? 'active' : ''} onClick={() => handlePerCategoryBtn(1)}>HTML</button>
         <button className={btnPerCategory == 2 ? 'active' : ''} onClick={() => handlePerCategoryBtn(2)}>CSS</button>
-        <button className={btnPerCategory == 1 ? 'active' : ''} onClick={() => handlePerCategoryBtn(1)}>JavaScript</button>
+        <button className={btnPerCategory == 3 ? 'active' : ''} onClick={() => handlePerCategoryBtn(3)}>JavaScript</button>
         <button className={btnPerCategory == 4 ? 'active' : ''} onClick={() => handlePerCategoryBtn(4)}>ReactJs</button>
         <button className={btnPerCategory == 5 ? 'active' : ''} onClick={() => handlePerCategoryBtn(5)}>Git</button>
+      </div> */}
       </div>
     </section>
   );
--- app/my-blog/page.tsx
+++ app/my-blog/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image';
 import './myBlog.scss';
-import Link from 'next/link';
 import BlogList from '../components/blogList/BlogList';
 import SearchBlog from '../components/searchBlog/SearchBlog';
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: 'Web Development Portfolio: Crafting Impactful Digital Experiences',
+  description: 'Explore a web developer portfolio showcasing skills, creativity, and dedication in crafting impactful digital experiences. Discover coding passion and user-friendly solutions.',
+}
 const MyBlogPage: React.FC = () => {
   return (
     <section className="blog">
